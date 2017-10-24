910cddacace8776ea00cc4f53cd3a839fd2ab38e
phploc <32526361+phploc@users.noreply.github.com>
2017-10-24 17:14:24 +0300

    Оптимизация

--- multiquery.php
+++ multiquery.php
@@ -5,24 +5,30 @@ if ($mysqli->connect_error) {
             . $mysqli->connect_error);
 }
 
-$json_string=file_get_contents('qa.json');
-$normal_string=json_decode($json_string,true);
+$normal_string=json_decode(file_get_contents('qa.json'),true);
 
 $string='';
 foreach($normal_string as $v){
+	$esc_ques = $mysqli->real_escape_string($v['q']);
+	$esc_answ = $mysqli->real_escape_string($v['a']);
 	//$string.="INSERT INTO `FAQ` (`id`, `quest`, `answ`) VALUES (NULL, '{$v['q']}', '{$v['a']}');";   //просто добавление
-	$string.="INSERT INTO FAQ(quest,answ) SELECT '{$v['q']}','{$v['a']}' WHERE NOT EXISTS (SELECT 1 FROM FAQ WHERE quest = '{$v['q']}' AND answ = '{$v['a']}' LIMIT 1) LIMIT 1;"; //добавление с проверкой
+	$string.="INSERT INTO FAQ(quest,answ) SELECT '{$esc_ques}','{$esc_answ}' WHERE NOT EXISTS (SELECT 1 FROM FAQ WHERE quest = '{$esc_ques}' AND answ = '{$esc_answ}' LIMIT 1) LIMIT 1;"; //добавление с проверкой
 }
 
-$result=$mysqli->multi_query($string);
+$mysqli->multi_query($string);
 while ($mysqli->next_result());
- 
-$query="SELECT * FROM FAQ";
-if ($stmt = $mysqli->prepare($query)) {
+
+$resul=$mysqli->query("SELECT * FROM FAQ");
+while($row=$resul->fetch_row()){
+	echo 'Вопрос '.$row[0].' :'.$row[1].'; <br> Ответ:'.$row[2].'<br>';
+}
+$mysqli->close();
+/*
+if ($stmt = $mysqli->prepare("SELECT * FROM FAQ")) {
     $stmt->execute();
     $stmt->bind_result($id, $quest, $answ);
     while ($stmt->fetch()) {
 		echo 'Вопрос '.$id.' :'.$quest.'; <br> Ответ:'.$answ.'<br>';
     }
     $stmt->close();
-}
+}*/
