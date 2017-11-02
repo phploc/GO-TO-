8cea89e1a4ab5df8a8bd7828c7c0688d9b3664f0
phploc <32526361+phploc@users.noreply.github.com>
2017-11-02 19:22:27 +0300

    Иправления
    Исправлена запись доп. контактной информации добавлено преобразование HTML сущности в имени

--- parse_info.php
+++ parse_info.php
@@ -1,19 +1,25 @@
 <?php
 
-//$mysqli = new mysqli('Localhost', 'reactor_localmar', 'DBreset', 'reactor_localmar');
+
+$start_script=microtime(true);
+
 $mysqli = new mysqli('127.0.0.1', 'root', '', 'localmart');
+//$mysqli = new mysqli('Localhost', 'reactor_localmar', 'DBreset', 'reactor_localmar');
 if ($mysqli->connect_error) {
     die('Ошибка подключения (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
 
-$DB_urls= $mysqli->query("SELECT id,url FROM urls WHERE category is NULL LIMIT 5");
+$DB_urls= $mysqli->query("SELECT id,url FROM urls WHERE category is NULL LIMIT 10");
 while($res=$DB_urls->fetch_assoc()){
 $url=$res['url'];
 $ID=$res['id'];
 usleep(400000);
 
+unset($contacts,$in);
+
 $in=parse_addinf($mysqli,$url);
+if($in['name']!=NULL){
 $esc_name = $mysqli->real_escape_string($in['name']);
 $esc_prise = $mysqli->real_escape_string($in['prise']);
 $esc_valute = $mysqli->real_escape_string($in['id_valute']);
@@ -21,19 +27,74 @@ $esc_img = $mysqli->real_escape_string($in['img']);
 $esc_body = $mysqli->real_escape_string($in['body']);
 $esc_town = $mysqli->real_escape_string($in['id_town']);
 $esc_id_cat = $mysqli->real_escape_string($in['id_cat']);
-
 $string.="UPDATE urls SET name='{$esc_name}', prise='{$esc_prise}', valute='{$esc_valute}', img_url='{$esc_img}', text_advert='{$esc_body}', town='{$esc_town}', category='{$esc_id_cat}' WHERE id={$ID} LIMIT 1;"; //добавление с проверкой
+}
+
+$contacts=parse_contact($url);
+if($contacts['number']!=NULL){
+$esc_numb = $mysqli->real_escape_string($contacts['number']);
+$string.="UPDATE urls SET contact_number ='{$esc_numb}' WHERE id={$ID} LIMIT 1;";
+}
+
+if($contacts['skype']!=NULL || $contacts['icq']!=NULL){
+$adds='';
+	if($contacts['skype']!=NULL)
+		$adds.='Skype:'.$contacts['skype'];
+	if($contacts['icq']!=NULL)
+		$adds.=' ICQ:'.$contacts['icq'];
+		
+$esc_adds = $mysqli->real_escape_string($adds);
+
+$string.="UPDATE urls SET contact_adds ='{$esc_adds}' WHERE id={$ID} LIMIT 1;";
+}
+//var_dump($contacts);
+
 //var_dump($in);
-//var_dump($res);
+
 }
 $mysqli->multi_query($string);
 	printf("Errormessage: %s\n", $mysqli->error);
 	$mysqli->close();
-/*
-$url="http://minsk.localmart.by/item/pylesos_bosch_so_skidkoy/98906035";/*'http://minsk.localmart.by/item/showContactInfo?id=98906035'*/
+
+$time_script=(microtime(true)-$start_script)*1000;
+echo "<br> программа выполнялась $time_script милисекунд";
+
+function parse_contact($url){
+preg_match_all('@//(.+?)/.+/(\d+)$@', $url, $matches, PREG_SET_ORDER, 0);
+$headers=array("Host: {$matches[0][1]}",'Accept: */*','X-Requested-With: XMLHttpRequest','User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36','Content-Type: application/x-www-form-urlencoded; charset=UTF-8',"Referer: {$url}",'Accept-Encoding: gzip, deflate, sdch','Accept-Language: ru,en-US;q=0.8,en;q=0.6','Cookie: _ym_uid=1508877069276947460; PHPSESSID=l3nukful7m64ia96827i1p2rv3; _ym_isad=2; show_categories=0; _ga=GA1.2.1453903692.1508877068; _gid=GA1.2.502684815.1509370632; _ym_visorc_21642088=w');
+$quest="http://{$matches[0][1]}/item/showContactInfo?id={$matches[0][2]}";
+$e=curl_contact($quest,$headers);
+preg_match_all('@<p.+?class="value">.+?<@', $e, $cont, PREG_PATTERN_ORDER, 0);
+
+foreach($cont[0] as $v){
+preg_match_all('@>(.+?)<@', $v, $matc, PREG_PATTERN_ORDER, 0);
+$trash=array('+','(',')',' ','-');
+$i=0;
+if(stristr($matc[1][0], 'skype') == TRUE)
+	$contact['skype']=$matc[1][1];
+elseif(stristr($matc[1][0], 'icq') == TRUE)
+	$contact['icq']=$matc[1][1];
+else
+	$contact['number']=str_replace($trash,'',$matc[1][0]);
+}
+return $contact;
+}
 
 
-
+function curl_contact($url,$headers) {
+	$q=curl_init($url);
+	curl_setopt($q, CURLOPT_HEADER, false);
+	curl_setopt($q, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($q, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($q, CURLOPT_MAXREDIRS, 1);
+	curl_setopt($q, CURLOPT_SSL_VERIFYHOST, false); 
+	curl_setopt($q, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($q, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($q, CURLOPT_ENCODING, ''); 
+	$z=curl_exec($q);
+	curl_close($q);
+	return $z;
+}
 
 
 
@@ -44,7 +105,7 @@ function parse_addinf(&$mysqli,$url){
 	$e= curl($url,$url);
 	
 	preg_match_all('@<span class="user-name">(.+?)</span>@', $e, $name, PREG_SET_ORDER, 0);
-	$info['name']=$name[0][1];
+	$info['name']=htmlspecialchars_decode($name[0][1],ENT_QUOTES);
 	
 	preg_match_all('@<span class="price main">(.+?)</span>@sm', $e, $matches, PREG_SET_ORDER, 0);
 	if($matches[0][1]=='Договорная'){
